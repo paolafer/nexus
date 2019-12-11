e64fed6bd2524d86f2cdee69968e60058a474e48
Paola Ferrario <paola.ferrario@gmail.com>
2019-12-11 20:05:40 +0100

    Add some randomness.

--- source/tests/unit_test/BoxPointSamplerTests.cc
+++ source/tests/unit_test/BoxPointSamplerTests.cc
@@ -1,37 +1,41 @@
 #include <BoxPointSampler.h>
+#include <Randomize.hh>
 
 #include <catch2/catch.hpp>
 
 TEST_CASE("BoxPointSampler") {
-  auto a = 10.;
-  auto b = 10.;
-  auto c = 10.;
-  auto thick = 1.;
-  auto sampler = nexus::BoxPointSampler(a, b, c, thick);
-  auto vertex  = sampler.GenerateVertex("WHOLE_VOL");
-  if (((vertex.x() >  a/2.)         & (vertex.x() <  a/2. + thick)) ||
-      ((vertex.x() > -a/2. - thick) & (vertex.x() < -a/2.))  ) {
-    REQUIRE(vertex.y() > -b/2 - thick);
-    REQUIRE(vertex.y() <  b/2 + thick);
-    REQUIRE(vertex.z() > -c/2 - thick);
-    REQUIRE(vertex.z() <  c/2 + thick);
-  }
 
-  if (((vertex.y() >  b/2.)         & (vertex.y() <  b/2. + thick)) ||
-      ((vertex.y() > -b/2. - thick) & (vertex.y() < -b/2.))  ) {
-    REQUIRE(vertex.x() > -a/2 - thick);
-    REQUIRE(vertex.x() <  a/2 + thick);
-    REQUIRE(vertex.z() > -c/2 - thick);
-    REQUIRE(vertex.z() <  c/2 + thick);
-  }
+  auto a = 2 + 20 * G4UniformRand();
+  auto b = a + 2;
+  auto c = a + 3;
 
-  if (((vertex.z() >  c/2.)         & (vertex.z() <  c/2. + thick)) ||
-      ((vertex.z() > -c/2. - thick) & (vertex.z() < -c/2.))  ) {
-    REQUIRE(vertex.x() > -a/2 - thick);
-    REQUIRE(vertex.x() <  a/2 + thick);
-    REQUIRE(vertex.y() > -b/2 - thick);
-    REQUIRE(vertex.y() <  b/2 + thick);
-  }
+  for (G4int i=0; i<20; i++) {
+    auto thick = G4UniformRand();
+    auto sampler = nexus::BoxPointSampler(a, b, c, thick);
+    auto vertex  = sampler.GenerateVertex("WHOLE_VOL");
+    if (((vertex.x() >  a/2.)         & (vertex.x() <  a/2. + thick)) ||
+	((vertex.x() > -a/2. - thick) & (vertex.x() < -a/2.))  ) {
+      REQUIRE(vertex.y() > -b/2 - thick);
+      REQUIRE(vertex.y() <  b/2 + thick);
+      REQUIRE(vertex.z() > -c/2 - thick);
+      REQUIRE(vertex.z() <  c/2 + thick);
+    }
 
+    if (((vertex.y() >  b/2.)         & (vertex.y() <  b/2. + thick)) ||
+	((vertex.y() > -b/2. - thick) & (vertex.y() < -b/2.))  ) {
+      REQUIRE(vertex.x() > -a/2 - thick);
+      REQUIRE(vertex.x() <  a/2 + thick);
+      REQUIRE(vertex.z() > -c/2 - thick);
+      REQUIRE(vertex.z() <  c/2 + thick);
+    }
+
+    if (((vertex.z() >  c/2.)         & (vertex.z() <  c/2. + thick)) ||
+	((vertex.z() > -c/2. - thick) & (vertex.z() < -c/2.))  ) {
+      REQUIRE(vertex.x() > -a/2 - thick);
+      REQUIRE(vertex.x() <  a/2 + thick);
+      REQUIRE(vertex.y() > -b/2 - thick);
+      REQUIRE(vertex.y() <  b/2 + thick);
+    }
+  }
 
 }
