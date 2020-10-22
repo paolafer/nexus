dec4c3430e3c64288405910b40ce67e42cf76ec1
Paola Ferrario <paola.ferrario@gmail.com>
2020-10-22 18:36:50 +0200

    Update LYSO rise time.

--- source/materials/OpticalMaterialProperties.cc
+++ source/materials/OpticalMaterialProperties.cc
@@ -1321,7 +1321,8 @@ const G4int sc_entries = 63;
 
   mpt->AddProperty("RINDEX", ri_energy, rindex, ri_entries);
   mpt->AddProperty("FASTCOMPONENT", sc_energy, intensity, sc_entries);
-  mpt->AddConstProperty("FASTSCINTILLATIONRISETIME", 0.09*ns); // = 0.773703*ns 1.7/Ln(9)
+  // S Seifert et al 2012 JINST 7 P09004
+  mpt->AddConstProperty("FASTSCINTILLATIONRISETIME", 0.072*ns); // = 0.773703*ns 1.7/Ln(9)
   // mpt->AddProperty("SLOWCOMPONENT", sc_energy, intensity, sc_entries);
   mpt->AddConstProperty("SCINTILLATIONYIELD", 32000./MeV);
   // mpt->AddConstProperty("SCINTILLATIONYIELD", 100./MeV);
